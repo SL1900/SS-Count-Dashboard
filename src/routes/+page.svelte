c2ff754853da8e7262e5ad90fa5694827bcb797a
SL1900 <43700351+SL1900@users.noreply.github.com>
2024-12-27 18:03:36 +0900

    Added time sliders (slow and buggy right now)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,122 +2,113 @@
     import { CategoryScale, Chart, LinearScale, LineController, LineElement, PointElement, TimeScale, Tooltip } from "chart.js";
     import "chartjs-adapter-luxon";
     import ChartDataLabels from "chartjs-plugin-datalabels";
+    import RangeSlider from "svelte-range-slider-pips";
     // import "chartjs-plugin-zoom";
     import { onMount } from "svelte";
-
-    // import data from "/total_messages.json";
-    let data: {
-        content: string,
-        timestamp: number,
-        author: string
-    }[];
-
+    import { FormatTimestamp } from "$lib/utils";
 
     let ctx: CanvasRenderingContext2D;
     let chartCanvas: HTMLCanvasElement;
 
-    let special_numbers: {author: string, value: number}[] = [];
-    
+    let data: {content: string, timestamp: number, author: string}[];
+    let timestamps: number[] = [];
+    let labels: string[] = [];
+    let values: number[] = [];
+    let authors: string[] = [];
+
+    //Time slider
+    let slider_values = [0,0];
+    let slider_min = 0;
+    let slider_max = 0;
+
+    let chart: Chart;
+
+    function UpdateData(from: number = 0, to: number = 0, each: number = 3) {
+        let data_part = data.filter(m=>{
+            return ((+m.content % each == 0) || (+m.content % 1000 == 0)) &&
+            ( +(new Date(m.timestamp)) > from && +(new Date(m.timestamp)) < (to || Number.POSITIVE_INFINITY))
+        ;});
+
+        labels = data_part.map(m=>(new Date(m.timestamp)).toISOString() );
+        timestamps = data_part.map(m=>+(new Date(m.timestamp)));
+        values = data_part.map(m=>+m.content);
+        authors = data_part.map(m=>m.author);
+    }
+
+    let last_chart_update = 0;
+    $: {
+        slider_values;
+        if(data && (Date.now() - last_chart_update) > 500) {
+            last_chart_update = Date.now();
+
+            let each = 3;
+            if(values.length < 2000) each = 1;
+            else if(values.length >= 2000 && values.length < 4000) each = 2;
+            else if(values.length >= 4000 && values.length < 8000) each = 4;
+            else if(values.length >= 8000) each = 8;
+
+            UpdateData(slider_values[0], slider_values[1], each);
+            UpdateCrosses();
+            chart.data.datasets[0].data = values;
+            // //@ts-ignore
+            chart.data.labels = labels;
+            chart.update();
+        }
+    }
+
+    function UpdateCrosses() {
+        //@ts-ignore
+        chart.config.data.datasets[0]["pointStyle"] = (new Array(values.length)).fill("circle");
+        //@ts-ignore
+        chart.config.data.datasets[0]["pointRadius"] = (new Array(values.length)).fill(1);
+        for(let i = 0; i < values.length; i++) {
+            if(+values[i] % 1000 == 0) {
+                //@ts-ignore
+                chart.config.data.datasets[0]["pointStyle"][i] = "cross";
+                //@ts-ignore
+                chart.config.data.datasets[0]["pointRadius"][i] = 10;
+            }
+        }
+    }
+
     onMount(async () => {
         data = await (await fetch("/total_messages.json")).json();
 
-        let data_part = data.filter(m=>{return (+m.content % 3 == 0) || (+m.content % 1000 == 0);});
+        UpdateData();
 
-        let labels = data_part.map(m=>(new Date(m.timestamp)).toISOString() ).reverse();
-        let timestamps = data_part.map(m=>+(new Date(m.timestamp))).reverse();
-        let values = data_part.map(m=>m.content).reverse();
-        let authors = data_part.map(m=>m.author).reverse();
+        let slider_hours_start = Math.floor(timestamps[0] / (60 * 60 * 1000));
+        let slider_hours_end = Math.ceil(timestamps[timestamps.length - 1] / (60 * 60 * 1000));
 
-        // UpdateSpecial();
+        slider_min = slider_hours_start * 60 * 60 * 1000;
+        slider_max = slider_hours_end * 60 * 60 * 1000;
 
-        let max_diff = 0;
-        let min_diff = 100000000;
-        let SLICE_SIZE = 200;
-        let COLORS = {
-            RED: [255,0,0],
-            GREEN: [0,255,0],
-            BLUE: [0,0,255]
-        };
-
-        let TEMPERATURE = (new Array(values.length)).fill("black");
-
-        for(let i = 0; i < timestamps.length; i++) {
-            let slice = timestamps.slice(Math.max(25,i-SLICE_SIZE/2), Math.min(timestamps.length - 1, i+SLICE_SIZE/2)).map(v=>+v);
-            let diff = Math.max(...slice) - Math.min(...slice);
-            if(max_diff < diff) max_diff = diff;
-            if(min_diff > diff) min_diff = diff;
-        }
-        for(let i = 0; i < timestamps.length; i++) {
-            let slice = timestamps.slice(Math.max(25,i-SLICE_SIZE/2), Math.min(timestamps.length - 1, i+SLICE_SIZE/2)).map(v=>+v);
-            // let diff = Math.max(...slice) - Math.min(...slice);
-            let diff = slice[slice.length - 1] - slice[0];
-            // console.log(slice)
-
-            let percent = (diff - min_diff) / (max_diff - min_diff);
-            // let percent = (diff - min_diff) / (max_diff - min_diff);
-
-            // console.log(diff, min_diff, max_diff, percent)
-
-            let color = pickHex(COLORS.GREEN, COLORS.RED, percent);
-            // console.log("#" + color.join(""), diff, max_diff, percent)
-
-            //@ts-ignore
-            // chart.config.data.datasets[0]["borderColor"][i] = "#" + color.join("");
-            //@ts-ignore
-            // chart.config.data.datasets[0]["backgroundColor"][i] = "#" + color.join("");
-            //@ts-ignore
-            // chart.config.data.datasets[0]["borderColor"][i] = "#00ff00";
-
-            // TEMPERATURE[i] = "#" + color.join("");
-            // console.log(`rgb(${color[0]},${color[1]},${color[2]}) -- ${percent}`);
-            TEMPERATURE[i] = `rgb(${color[0]},${color[1]},${color[2]})`;
-        }
+        slider_values = [slider_min, slider_max];
 
         ctx = chartCanvas.getContext("2d") as CanvasRenderingContext2D;
         Chart.register(ChartDataLabels);
         Chart.register(LineController, LineElement, PointElement, CategoryScale, LinearScale);
         Chart.register(TimeScale, Tooltip);
 
-        let chart = new Chart(ctx, {
+        chart = new Chart(ctx, {
             type: "line",
             data: {
-                labels,
+                labels: labels,
                 datasets: [{
                     label: "count",
                     data: values,
-                    pointRadius: 10,
+                    pointRadius: (new Array(values.length)).fill(1),
                     yAxisID: "y1",
-                    pointStyle: (new Array(values.length)).fill(false),
-                    // borderColor: (new Array(values.length)).fill(false),
+                    pointStyle: (new Array(values.length)).fill("circle"),
                     borderColor: "#36a2eb",
                     borderWidth: 1,
-                    // backgroundColor: (new Array(values.length)).fill(null),
-                    // borderColor: (context) => {
-                    //     const chart = context.chart;
-                    //     const {ctx, chartArea} = chart;
-                    //
-                    //     if(!chartArea) { return; }
-                    //
-                    //     let gradient;
-                    //     gradient = ctx.createLinearGradient(chartArea.left, 0, chartArea.right, 0);
-                    //     for(let i = 0; i < TEMPERATURE.length; i = i + 1000) {
-                    //         //
-                    //         gradient.addColorStop(i / TEMPERATURE.length, TEMPERATURE[i]);
-                    //     }
-                    //
-                    //     return gradient;
-                    // }
-                },
-                ]
+                }]
             },
             options: {
                 responsive: true,
                 maintainAspectRatio: true,
                 aspectRatio: 2,
                 resizeDelay: 100,
-                onResize(chart, _) {
-                    chart.resize();
-                },
+                onResize(chart, _) { chart.resize(); },
                 scales: {
                     x: { type: "time" },
                     y1: { ticks: { stepSize: 100 }, position: "left" },
@@ -126,52 +117,29 @@
                 plugins: {
                     datalabels: {
                         align: "left",
+                        font: function(context) {
+                            return { size: context.chart.width < 800 ? 8 : 12 }
+                        },
                         formatter: function(value, context) {
-                            // console.log(context.dataset)
-                            // console.log(value)
                             return (context.dataset.data[context.dataIndex] as number || 0) % 1000 == 0 ? value : "";
                         }
                     },
-                    // zoom: {
-                    //     zoom: {
-                    //         wheel: {
-                    //             enabled: true
-                    //         },
-                    //     }
-                    // }
-                }
+                    tooltip: {
+                        callbacks: {
+                            label: function(item) {
+                                let value = ["Count: " + values[item.dataIndex]];
+                                value.push("Author: " + authors[item.dataIndex]);
+                                return value;
+                            }
+                        }
+                    }
+                },
             }
         });
 
-        for(let i = 0; i < values.length; i++) {
-            if(+values[i] % 1000 == 0) {
-                //@ts-ignore
-                chart.config.data.datasets[0]["pointStyle"][i] = "cross";
-            }
-        }
+        UpdateCrosses();
     });
-    function pickHex(color1: number[], color2: number[], weight: number) {
-        var w1 = weight;
-        var w2 = 1 - w1;
-        var rgb = [Math.round(color1[0] * w1 + color2[0] * w2),
-        Math.round(color1[1] * w1 + color2[1] * w2),
-        Math.round(color1[2] * w1 + color2[2] * w2)];
-        // return rgb.map(e=>e.toString(16).padStart(2,"0"));
-        return rgb.map(e=>e.toString());
-    }
 
-    // function UpdateSpecial() {
-    //     for(let i = 0; i < values.length; i++) {
-    //         if(+values[i] % 1000 !== 0) continue;
-    //
-    //         special_numbers.push({
-    //             author: authors[i],
-    //             value: Number.parseInt(values[i])
-    //         });
-    //     }
-    //     //@ts-ignore
-    //     special_numbers = special_numbers;
-    // }
 </script>
 
 <div class="w-full gap-3 text-center m-2 font-bold text-4xl page-title flex justify-center">
@@ -179,7 +147,8 @@
         <div>StellaSora</div>
         <div class="text-xs flex justify-center under-text underline">discord server</div>
     </a>
-    <div> - #counting</div>
+    <div>-</div>
+    <div>#counting</div>
 </div>
 <main class="flex max-h-screen pr-2 pl-2 flex-wrap">
     <!-- <div class="w-64 flex flex-col"> -->
@@ -197,6 +166,13 @@
     <!-- </div> -->
     <div class="canvas-container flex-[3_3_0%] w-full xl:w-1/2 relative border-2 border-sky-300 p-2 m-2">
         <canvas bind:this={chartCanvas}></canvas>
+        <div class="range-container flex flex-col">
+            <div class="time-labels flex justify-between">
+                <div class="start_time">{FormatTimestamp(slider_values[0])}</div>
+                <div class="end_time">{FormatTimestamp(slider_values[1])}</div>
+            </div>
+            <RangeSlider pips pipstep={60 * 1000} step={3_600_000 / 4} range pushy bind:values={slider_values} min={slider_min} max={slider_max} />
+        </div>
     </div>
     <iframe class="flex-1 min-h-96" title="KarmaSkeleton" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRdkxOhKVUS0H831xeSF4J5HLeMP-vcdNoDt6y64n3hHC1lVnuLhfD-iIyqKSlqQmMR4pSk7C-tWrFn/pubhtml?widget=true&amp;headers=false"></iframe>
 </main>
